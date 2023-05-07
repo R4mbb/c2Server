400ae4619ca78be16f2b192f60355c7a8aed3049
R4mbb <48462655+R4mbb@users.noreply.github.com>
2023-05-07 23:44:41 +0900

    Create vim-plugin.sh

---
+++ install/vim-plugin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+wget https://raw.githubusercontent.com/jjeaby/jscript/master/.vimrc
+
+# vim .vimrc
+# :PluginInstall
